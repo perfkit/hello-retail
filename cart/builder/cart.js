5fe4bb82021d7d80a53a85c08cbfa18294c1d6f4
Wang, Ashley Q <ashley.wang2@nordstrom.com>
2018-06-20 11:42:36 -0700

    created cart dynamoDB to store items added to cart. Created lambda to read cart events from the stream and write to the database.

--- cart/builder/cart.js
+++ cart/builder/cart.js
@@ -3,14 +3,14 @@
 const aws = require('aws-sdk') // eslint-disable-line import/no-unresolved, import/no-extraneous-dependencies
 const KH = require('kinesis-handler')
 
-const eventSchema = require('./retail-stream-schema-egress.json')
+const eventSchema = require('./retail-stream-schema-ingress.json')
 const productCartSchema = require('./product-cart-schema.json')
 
 const constants = {
   // self
   MODULE: 'cart/builder/cart.js',
   // methods
-  METHOD_PUT_PRODUCT: 'putProduct',
+  METHOD_PUT_CART: 'putCart',
   // resources
   TABLE_CART_NAME: process.env.TABLE_CART_NAME,
 }
@@ -22,7 +22,7 @@ const dynamo = new aws.DynamoDB.DocumentClient()
 const impl = {
   // TODO update the below comment to reflect cart schema
   /**
-   * Put the carted product in to the dynamo cart catalog.  Example event:
+   * Put the carted product in to the dynamo cart database.  Example event:
    * {
    *   "schema": "com.nordstrom/retail-stream/1-0-0",
    *   "origin": "hello-retail/product-producer-automation",
@@ -32,11 +32,10 @@ const impl = {
    *     "id": "4579874"
    *   }
    * }
-   * @param event The product to put in the catalog.
+   * @param event The product to put in the cart.
    * @param complete The callback to inform of completion, with optional error parameter.
    */
-// TODO check this method
-  putProduct: (event, complete) => {
+  putCart: (event, complete) => {
     const updated = Date.now()
     let priorErr
     const updateCallback = (err) => {
@@ -47,9 +46,9 @@ const impl = {
           priorErr = false
         }
       } else if (priorErr && err) { // second update result, if an error was previously received and we have a new one
-        complete(`${constants.METHOD_PUT_PRODUCT} - errors updating DynamoDb: ${[priorErr, err]}`)
+        complete(`${constants.METHOD_PUT_CART} - errors updating DynamoDb: ${[priorErr, err]}`)
       } else if (priorErr || err) {
-        complete(`${constants.METHOD_PUT_PRODUCT} - error updating DynamoDb: ${priorErr || err}`)
+        complete(`${constants.METHOD_PUT_CART} - error updating DynamoDb: ${priorErr || err}`)
       } else { // second update result if error was not previously seen
         complete()
       }
@@ -57,35 +56,23 @@ const impl = {
     const dbParamsCart = {
       TableName: constants.TABLE_CART_NAME,
       Key: {
-        id: event.data.id, // TODO change to match cart schema; primary key is customer id
+          userId: event.origin,
       },
-      UpdateExpression: [ // TODO change to match cart schema
+      UpdateExpression: [
         'set',
         '#c=if_not_exists(#c,:c),',
         '#cb=if_not_exists(#cb,:cb),',
-        '#u=:u,',
-        '#ub=:ub,',
-        '#b=:b,',
-        '#n=:n,',
-        '#d=:d,',
+        '#id=:id',
       ].join(' '),
       ExpressionAttributeNames: {
         '#c': 'created',
         '#cb': 'createdBy',
-        '#u': 'updated',
-        '#ub': 'updatedBy',
-        '#b': 'brand',
-        '#n': 'name',
-        '#d': 'description',
+        '#id': 'productId',
       },
       ExpressionAttributeValues: {
         ':c': updated,
         ':cb': event.origin,
-        ':u': updated,
-        ':ub': event.origin,
-        ':b': event.data.brand,
-        ':n': event.data.name,
-        ':d': event.data.description,
+        ':id': event.data.id,
       },
       ReturnValues: 'NONE',
       ReturnConsumedCapacity: 'NONE',
@@ -95,7 +82,7 @@ const impl = {
   },
 }
 
-kh.registerSchemaMethodPair(productCartSchema, impl.putProduct)
+kh.registerSchemaMethodPair(productCartSchema, impl.putCart)
 
 module.exports = {
   processKinesisEvent: kh.processKinesisEvent.bind(kh),
