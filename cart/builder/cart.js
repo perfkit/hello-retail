75eab6c855ec1381a569c6a79de7f1aab19f9cd5
Wang, Ashley Q <ashley.wang2@nordstrom.com>
2018-07-05 10:12:31 -0700

    remove button method

--- cart/builder/cart.js
+++ cart/builder/cart.js
@@ -4,13 +4,15 @@ const aws = require('aws-sdk') // eslint-disable-line import/no-unresolved, impo
 const KH = require('kinesis-handler')
 
 const eventSchema = require('./retail-stream-schema-ingress.json')
-const productCartSchema = require('./product-cart-schema.json')
+const addCartSchema = require('./cart-add-schema.json')
+const removeCartSchema = require('./cart-remove-schema.json')
 
 const constants = {
   // self
   MODULE: 'cart/builder/cart.js',
   // methods
   METHOD_PUT_CART: 'putCart',
+  METHOD_REMOVE_CART: 'removeCart',
   // resources
   TABLE_CART_NAME: process.env.TABLE_CART_NAME,
 }
@@ -25,10 +27,10 @@ const impl = {
    * {
    *   "schema": "com.nordstrom/retail-stream/1-0-0",
    *   "data": {
-   *     "schema": "com.nordstrom/product/cart/1-0-0",
+   *     "schema": "com.nordstrom/product/add-cart/1-0-0",
    *     "id": "4579874"
    *   }
-   *   "origin": "hello-retail/cart-automation",
+   *   "origin": "hello-retail/web-client-cart-add/amzn1.account.FFB43IREIOXFBHWJERAQCI9M5JCJ/Jane Smith",
    * }
    * @param event The product to put in the cart.
    * @param complete The callback to inform of completion, with optional error parameter.
@@ -54,7 +56,7 @@ const impl = {
     const dbParamsCart = {
       TableName: constants.TABLE_CART_NAME,
       Key: {
-          userId: event.origin.slice(event.origin.lastIndexOf(".")+1, event.origin.lastIndexOf("/")),
+          userId: event.origin.slice(event.origin.lastIndexOf(".")+1, event.origin.lastIndexOf("/")), // example userId: FFB43IREIOXFBHWJERAQCI9M5JCJ
           productId: event.data.id,
       },
       UpdateExpression: [
@@ -73,8 +75,8 @@ const impl = {
       },
       ExpressionAttributeValues: {
         ':c': Date.now(),
-        ':u': Date.now(),
-        ':fn': event.origin.slice(event.origin.lastIndexOf("/")+1),
+        ':u': Date.now().toString(),
+        ':fn': event.origin.slice(event.origin.lastIndexOf("/")+1), // example friendlyName: Jane Smith
         ':q': 1,
       },
       ReturnValues: 'NONE',
@@ -84,9 +86,52 @@ const impl = {
     console.log(dbParamsCart)
     dynamo.update(dbParamsCart, updateCallback)
   },
+
+  /**
+   * Remove the product from the dynamo cart database.  Example event:
+   * {
+   *   "schema": "com.nordstrom/retail-stream/1-0-0",
+   *   "data": {
+   *     "schema": "com.nordstrom/product/remove-cart/1-0-0",
+   *     "id": "4579874"
+   *   }
+   *   "origin": "hello-retail/web-client-cart-remove/amzn1.account.FFB43IREIOXFBHWJERAQCI9M5JCJ/Jane Smith",
+   * }
+   * @param event The product to remove from the cart.
+   * @param complete The callback to inform of completion, with optional error parameter.
+   */
+  removeCart: (event, complete) => {
+    let priorErr
+    const updateCallback = (err) => {
+      if (priorErr === undefined) { // first update result
+        if (err) {
+          console.log("err = ", err)
+          priorErr = err
+        } else {
+          priorErr = false
+        }
+      } else if (priorErr && err) { // second update result, if an error was previously received and we have a new one
+        complete(`${constants.METHOD_REMOVE_CART} - errors updating DynamoDb: ${[priorErr, err]}`)
+      } else if (priorErr || err) {
+        complete(`${constants.METHOD_REMOVE_CART} - error updating DynamoDb: ${priorErr || err}`)
+      } else { // second update result if error was not previously seen
+        complete()
+      }
+    }
+    const dbParamsCart = {
+      TableName: constants.TABLE_CART_NAME,
+      Key: {
+          userId: event.origin.slice(event.origin.lastIndexOf(".")+1, event.origin.lastIndexOf("/")),
+          productId: event.data.id,
+      },
+    }
+    console.log(dbParamsCart)
+    dynamo.delete(dbParamsCart, updateCallback)
+  },
 }
 
-kh.registerSchemaMethodPair(productCartSchema, impl.putCart)
+kh.registerSchemaMethodPair(addCartSchema, impl.putCart)
+kh.registerSchemaMethodPair(removeCartSchema, impl.removeCart)
 
 module.exports = {
   processKinesisEvent: kh.processKinesisEvent.bind(kh),
