cab735ba83a869cb1643afac2431ea1e651cacc7
Simon Trapp <simon.trapp@stud-mail.uni-wuerzburg.de>
2021-01-19 20:03:03 +0100

    use generic deployment bucket (untested)

--- web/config.js
+++ web/config.js
@@ -1,2 +1,11 @@
 module.exports = {
+ 'WebAppName': 'Hello Retail',
+ 'Stage': 'prod',
+ 'AWSRegion': 'us-east-1',
+ 'SessionName': 'hello-retail-website-prod',
+ 'WebAppRole': 'arn:aws:iam::410693349243:role/prodHelloRetailWebIdentity',
+ 'ProductCatalogTableName': 'prod-ProductCatalog-1',
+ 'ProductCategoryTableName': 'prod-ProductCategory-1',
+ 'CartTableName': 'prod-Cart',
+ 'EventWriterApi': '2bdge4err8',
 }
