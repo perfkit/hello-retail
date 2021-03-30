5383c04a292f3ac94d34e69d2effe9d527fedc48
Joel Scheuner <joel.scheuner.dev@gmail.com>
2021-03-30 12:09:10 +0000

    Migrate to run_k6 helper; Unify benchmark file

--- hello_retail_benchmark.py
+++ hello_retail_benchmark.py
@@ -1,39 +1,27 @@
 import base64
 import logging
-import os
-import random
+from pathlib import Path
 import re
-import time
-
-import requests
 
 BENCHMARK_CONFIG = """
-event_processing:
-  description: Hello Retail Benchmark for AWS.
+hello_retail:
+  description: Event-driven Retail application with HTTP api.
   provider: aws
   region: us-east-1
-  stage: prod
-  team: simont
-  company: uniwue
+  stage: dev
+  team: specrg
+  company: cloud
 """
 
 
 # API calls, ew=event-writer, pr=product-receive, pc=product-catalog
 
 
-# Util
-
-def encodeImage(source_filename, target_filename):
-  with open(source_filename, 'rb') as image:  # open binary file in read mode
-    image_64_encode = base64.b64encode(image.read())
-    with open(target_filename, "wb") as target_image_file:
-      target_image_file.write(image_64_encode)
-
-
 # SB calls
 
 def prepare(spec):
-  if not os.path.exists(os.path.dirname(__file__) + "/node_modules"):           # if no dependencies are installed, install dependencies
+  node_modules_path = Path(__file__).parent / 'node_modules'
+  if not node_modules_path.exists():  # if no dependencies are installed, install dependencies
     spec.run(f"./install.sh {spec['region']} {spec['stage']} {spec['company']} {spec['team']}", image='serverless_cli')
 
   log = spec.run(f"./deploy.sh {spec['region']} {spec['stage']} {spec['company']} {spec['team']}", image='serverless_cli')
@@ -62,12 +50,28 @@ def invoke(spec):
   image_target_file = "benchmark_images/snowdrop-base64.jpg"
   encodeImage(image_source_file, image_target_file)
 
-  out_options = f"--out csv={spec.workload_log_file()}"
-  spec.run(f'''k6 run {out_options} -e \"EVENT_WRITER_URL={spec['endpoint_event_writer_api']}\" \\
-    -e \"PRODUCT_CATALOG_URL={spec['endpoint_product_catalog_api']}\" \\
-    -e \"PHOTO_RECEIVE_URL={spec['endpoint_photo_receive_api']}\" \\
-    -e \"IMAGE_FILE={image_target_file}\" workload_script.js''', image = 'loadimpact/k6:0.30.0')
+  envs = {
+    'EVENT_WRITER_URL': spec['endpoint_event_writer_api'],
+    'PRODUCT_CATALOG_URL': spec['endpoint_product_catalog_api'],
+    'PHOTO_RECEIVE_URL': spec['endpoint_photo_receive_api'],
+    'IMAGE_FILE': image_target_file
+  }
+  spec.run_k6(envs)
+  # out_options = f"--out csv={spec.workload_log_file()}"
+  # spec.run(f'''k6 run {out_options} -e \"EVENT_WRITER_URL={spec['endpoint_event_writer_api']}\" \\
+  #   -e \"PRODUCT_CATALOG_URL={spec['endpoint_product_catalog_api']}\" \\
+  #   -e \"PHOTO_RECEIVE_URL={spec['endpoint_photo_receive_api']}\" \\
+  #   -e \"IMAGE_FILE={image_target_file}\" workload_script.js''', image = 'loadimpact/k6:0.30.0')
 
 
 def cleanup(spec):
   spec.run(f"./remove.sh {spec['region']} {spec['stage']} {spec['company']} {spec['team']}", image='serverless_cli')
+
+
+# Util
+
+def encodeImage(source_filename, target_filename):
+  with open(source_filename, 'rb') as image:  # open binary file in read mode
+    image_64_encode = base64.b64encode(image.read())
+    with open(target_filename, "wb") as target_image_file:
+      target_image_file.write(image_64_encode)
--- workload_script.js
+++ workload_script.js
@@ -1,47 +1,6 @@
 import { check } from 'k6'
 import http from 'k6/http'
-import imported_options from './workload_options.js'
 import crypto from 'k6/crypto'
-
-export let options = imported_options
-
-function randomIntBetween(min, max) { // min and max included
-	return Math.floor(Math.random() * (max - min + 1) + min);
-}
-
-// Copied from https://stackoverflow.com/a/55200387
-const byteToHex = [];
-
-for (let n = 0; n <= 0xff; ++n)
-{
-	const hexOctet = n.toString(16).padStart(2, '0');
-	byteToHex.push(hexOctet);
-}
-
-function hex(arrayBuffer)
-{
-	const buff = new Uint8Array(arrayBuffer);
-	const hexOctets = []; // new Array(buff.length) is even faster (preallocates necessary array size), then use hexOctets[i] instead of .push()
-
-	for (let i = 0; i < buff.length; ++i)
-		hexOctets.push(byteToHex[buff[i]]);
-
-	return hexOctets.join('');
-}
-
-function getXrayTraceHeader() {
-	// https://docs.aws.amazon.com/xray/latest/devguide/xray-services-apigateway.html
-	// 96-bit identifier
-	const trace_id = crypto.hexEncode(crypto.randomBytes(12))
-	const current_time_secs = Math.floor(Date.now() / 1000)
-
-	let ab = new ArrayBuffer(4)
-	let dv = new DataView(ab)
-	dv.setInt32(0, current_time_secs)
-	const current_time_hex = hex(ab)
-
-	return `Root=1-${current_time_hex}-${trace_id}`
-}
 
 // Init stage.
 const ew_url = __ENV.EVENT_WRITER_URL
@@ -248,3 +207,40 @@ function commitPhoto(pg_id, phone_number, item_id, image, xray_header) {
 	})
 }
 
+function randomIntBetween(min, max) { // min and max included
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getXrayTraceHeader() {
+	// https://docs.aws.amazon.com/xray/latest/devguide/xray-services-apigateway.html
+	// 96-bit identifier
+	const trace_id = crypto.hexEncode(crypto.randomBytes(12))
+	const current_time_secs = Math.floor(Date.now() / 1000)
+
+	let ab = new ArrayBuffer(4)
+	let dv = new DataView(ab)
+	dv.setInt32(0, current_time_secs)
+	const current_time_hex = hex(ab)
+
+	return `Root=1-${current_time_hex}-${trace_id}`
+}
+
+// Copied from https://stackoverflow.com/a/55200387
+const byteToHex = [];
+
+for (let n = 0; n <= 0xff; ++n)
+{
+	const hexOctet = n.toString(16).padStart(2, '0');
+	byteToHex.push(hexOctet);
+}
+
+function hex(arrayBuffer)
+{
+	const buff = new Uint8Array(arrayBuffer);
+	const hexOctets = []; // new Array(buff.length) is even faster (preallocates necessary array size), then use hexOctets[i] instead of .push()
+
+	for (let i = 0; i < buff.length; ++i)
+		hexOctets.push(byteToHex[buff[i]]);
+
+	return hexOctets.join('');
+}
